5cd3c3f8977b554dfec1df4fc80e7ff2326b6c44
ELuculent <ecjohns@live.com>
2020-04-29 21:01:28 +1000

    Handled colours in XML attributes

--- src/main/cab302/viewer/gui/Gui.java
+++ src/main/cab302/viewer/gui/Gui.java
@@ -1,7 +1,6 @@
 package cab302.viewer.gui;
 
 import cab302.viewer.exceptions.BadImageFormatException;
-import cab302.viewer.gui.actions.CloseAction;
 
 import javax.swing.*;
 import java.awt.*;
@@ -11,17 +10,42 @@ import java.awt.event.MouseListener;
 import java.io.IOException;
 import java.util.HashMap;
 
+import static cab302.viewer.util.HexToRGB.HexToRGB;
+
 public class Gui extends JFrame {
 
     private JFrame frame;
 
+
     public Gui(HashMap<String, String> xmlInfo) {
 
+        // ## Window Configuration ## \\
+        // Set window to fullscreen
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setUndecorated(true);
+        String bgString = xmlInfo.getOrDefault("bgColour", "#FFFFFF");
+        Color bgColour = HexToRGB(bgString);
+        getContentPane().setBackground(bgColour);
+
+        // Layout and closing
+        GridBagLayout layout = new GridBagLayout();
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLayout(layout);
+
+        // ## Utilities and Actions ## \\
         // Get close action
         Action close = new CloseAction("close", "Closes the app", KeyEvent.VK_ESCAPE);
 
         // Provide a way to decode and display images
         ImageGenerator imgGen = new ImageGenerator();
+
+
+        // ## GridBag Constraints ## \\
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.weighty = 1;
+        gbc.weightx = 1;
+
 
         // ## Input Handling ## \\
         // Set up root pane to handle inputs
@@ -57,12 +81,33 @@ public class Gui extends JFrame {
             }
         });
 
+
         // ## XML display ## \\
         // Create heading label if exists and add it to the window
         if (xmlInfo.containsKey("message")) {
-            JLabel titleText = new JLabel();
-            titleText.setText(xmlInfo.get("message"));
-            add(titleText);
+            JTextArea titleText = new JTextArea();
+            String[] messageStringArr = xmlInfo.get("message").split("%%%%");
+            titleText.setText(messageStringArr[0]);
+            titleText.setFont(new Font("Arial", Font.PLAIN, 84));
+            titleText.setForeground(
+                    HexToRGB(
+                            messageStringArr.length != 1 ?
+                                    (!messageStringArr[messageStringArr.length - 1].equals("") ?
+                                            messageStringArr[messageStringArr.length - 1] : "#000000")
+                                    : "#000000"
+                    )
+            );
+            titleText.setBackground(bgColour);
+
+            titleText.setEditable(false);
+            titleText.setLineWrap(true);
+            titleText.setWrapStyleWord(true);
+            titleText.setRows(3);
+            titleText.setColumns(25);
+
+            gbc.gridx = 0;
+            gbc.gridy = 0;
+            add(titleText, gbc);
         }
 
         // Create picture label if exists and add it to the window
@@ -74,25 +119,37 @@ public class Gui extends JFrame {
             } catch (IOException | BadImageFormatException e) {
                 e.printStackTrace();
             }
-            add(pictureLabel);
+
+            gbc.gridy = 1;
+            add(pictureLabel, gbc);
         }
 
         // Create information label if exists and add it to the window
         if (xmlInfo.containsKey("information")) {
-            JLabel informationText = new JLabel();
-            informationText.setText(xmlInfo.get("information"));
-            add(informationText);
+            JTextArea informationText = new JTextArea();
+            String[] infoStringArr = xmlInfo.get("information").split("%%%%");
+            informationText.setText(infoStringArr[0]);
+            informationText.setFont(new Font("Arial", Font.PLAIN, 36));
+            informationText.setForeground(
+                    HexToRGB(
+                            infoStringArr.length > 1 ?
+                                    (!infoStringArr[infoStringArr.length - 1].equals("") ?
+                                            infoStringArr[infoStringArr.length - 1] : "#000000")
+                                    : "#000000"
+                    )
+            );
+            informationText.setBackground(bgColour);
+
+            informationText.setEditable(false);
+            informationText.setLineWrap(true);
+            informationText.setWrapStyleWord(true);
+            informationText.setColumns(55);
+
+            gbc.gridy = 2;
+            add(informationText, gbc);
         }
 
-
-        // ## Window Configuration ## \\
-        // Set window to fullscreen
-        setExtendedState(JFrame.MAXIMIZED_BOTH);
-        setUndecorated(true);
-
-        // Layout and closing
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        setLayout(new GridLayout(3,3));
+        // Show window
         setVisible(true);
     }
 }
