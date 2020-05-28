ecb71052eb1fa07f4f3849950834b1cb490b5acf
ELuculent <ecjohns@live.com>
2020-05-28 12:47:39 +1000

    Completed commenting and method headers

--- src/main/cab302/viewer/gui/CloseAction.java
+++ src/main/cab302/viewer/gui/CloseAction.java
@@ -6,13 +6,23 @@ import java.awt.event.ActionEvent;
 
 public class CloseAction extends AbstractAction {
 
-    public CloseAction(String text, String desc, Integer mnemonicKey) {
+    /**
+     * Create a new CloseAction for use with the Billboard Viewer
+     * @param text Name of the action
+     * @param desc Description of action done
+     * @param mnemonicKey The key to bind the action to
+     */
+    CloseAction(String text, String desc, Integer mnemonicKey) {
 
         super(text);
         putValue(SHORT_DESCRIPTION, desc);
         putValue(MNEMONIC_KEY, mnemonicKey);
     }
 
+    /**
+     * The action's effect on the application
+     * @param e Action event. Unused.
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
         for (Frame frame : JFrame.getFrames()) {
--- src/main/cab302/viewer/util/HexToRGB.java
+++ src/main/cab302/viewer/util/HexToRGB.java
@@ -3,10 +3,17 @@ package cab302.viewer.util;
 import java.awt.*;
 
 public class HexToRGB {
+
+    /**
+     * Convert to Hexadecimal colour codes to RGB
+     * @param hexColour A hexidecimal colour of length 4 or 7
+     * @return RGB Color object
+     */
     public static Color HexToRGB(String hexColour) {
 
         Color returnedColour = null;
 
+        // If the hex colour string has length 4, convert to Color object
         if (hexColour.length() == 4)
             returnedColour = new Color(
                     Integer.valueOf(hexColour.substring(1,1), 255/16),
@@ -14,6 +21,8 @@ public class HexToRGB {
                     Integer.valueOf(hexColour.substring(3,3), 255/16)
             );
 
+        // Else, if the hex colour string has length 7, convert to Color
+        // object
         else if (hexColour.length() == 7)
             returnedColour = new Color(
                 Integer.valueOf(hexColour.substring(1,3), 16),
